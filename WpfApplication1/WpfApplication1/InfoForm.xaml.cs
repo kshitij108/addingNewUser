f1f231870c7182d7585e26590343c4be03527c37
kshitij108 <kshitij108@hotmail.com>
2012-12-07 15:11:51 -0500

    adding my project to git hub for the back up - first commit

---
+++ WpfApplication1/WpfApplication1/InfoForm.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for InfoForm.xaml
+    /// </summary>
+    public partial class InfoForm : Window
+    {
+        public InfoForm()
+        {
+            InitializeComponent();
+            TbFirstName.Focus();
+           
+        }
+/*
+    private void ComboBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (warningConditionCheckBox.IsChecked == true)
+        {
+            // Warn about this situation
+            var window = new MyDialog { Owner = GetWindow(this) };
+
+            // Confirm to proceed
+            if (window.ShowDialog() != true) {
+                e.Handled = true;
+            } 
+            else {
+                comboBox.IsDropDownOpen = true;
+            }
+        }
+    }
+  */   
+    private void ButtonOkClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+   
+        private void AddButton(object sender, RoutedEventArgs e)
+        {
+            var F = new MainWindow();
+            F.ShowDialog();
+        }
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindowHome = new MainWindow();
+            newWindowHome.ShowDialog();
+
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+	    {
+	        (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+          
+        }
+        private void First_Option(object sender, RoutedEventArgs e)
+        {
+
+            birthMonthDefault.Visibility = Visibility.Visible;
+            birthMonth.Visibility = Visibility.Hidden;
+            birthDate.Visibility = Visibility.Hidden;
+            pswdLable.Visibility = Visibility.Hidden;
+            pswdTextbox.Visibility = Visibility.Hidden;
+           
+        }
+        private void Second_Option(object sender, RoutedEventArgs e)
+        {
+
+            birthMonthDefault.Visibility = Visibility.Hidden;
+            birthMonth.Visibility = Visibility.Hidden;
+            birthDate.Visibility = Visibility.Hidden;
+            pswdLable.Visibility = Visibility.Visible;
+            pswdTextbox.Visibility = Visibility.Visible;
+           
+            
+        }
+        private void Third_Option(object sender, RoutedEventArgs e)
+        {
+
+            birthMonthDefault.Visibility = Visibility.Hidden;
+            birthMonth.Visibility = Visibility.Visible;
+            birthDate.Visibility = Visibility.Visible;
+            pswdLable.Visibility = Visibility.Hidden;
+            pswdTextbox.Visibility = Visibility.Hidden;
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+       
+      
+    }
+}
---
+++ WpfApplication1/WpfApplication1/optionsPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for optionsPage.xaml
+    /// </summary>
+    public partial class optionsPage : Window
+    {
+        public optionsPage()
+        {
+            InitializeComponent();
+
+        }
+       
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            var newWindow = new InfoForm();
+            newWindow.ShowDialog();
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+            var newWindowOptions = new optionsPage();  
+            newWindowOptions.ShowDialog();
+          
+        }
+        private void ViewUser_Click(object sender, RoutedEventArgs e)
+        {
+            var viewWindowOption = new viewAllUser();
+            viewWindowOption.ShowDialog();
+
+        }
+    }
+}
